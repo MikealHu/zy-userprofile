b3f9e52143c05381f9e16beee8322f4f069e21e4
hufenggang <michael.hu@jollycorp.com>
2019-12-31 10:47:29 +0800

    基础模块构建

---
+++ userprofile-common/src/main/java/cn/zy/userprofile/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * author: hufenggang
+ * email: michael.hu@jollycorp.com
+ * date: 2019/12/23 17:51
+ * 基础模块 java代码
+ */
+package cn.zy.userprofile.common;
---
+++ userprofile-common/src/main/scala/cn/zy/userprofile/common/package.scala
@@ -0,0 +1,11 @@
+package cn.zy.userprofile
+
+/**
+ * author: hufenggang
+ * email: michael.hu@jollycorp.com
+ * date: 2019/12/23 16:05
+ * 基础模块 scala代码
+ */
+package object common {
+
+}
