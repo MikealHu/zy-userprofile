fd18117c0e3721713ce285d6d4c756b3a63dd750
hufenggang <michael.hu@jollycorp.com>
2019-12-31 16:13:01 +0800

    构建数据服务层

---
+++ userprofile-dw/src/main/java/cn/zy/userprofile/dw/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/31 16:11
+ * 数据服务模块 java代码
+ */
+package cn.zy.userprofile.dw;
---
+++ userprofile-dw/src/main/scala/cn/zy/userprofile/dw/package.scala
@@ -0,0 +1,11 @@
+package cn.zy.userprofile
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/31 16:11
+ * 数据服务模块 scala代码
+ */
+package object dw {
+
+}
